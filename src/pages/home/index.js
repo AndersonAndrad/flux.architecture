1b253752735e1d50561e4300fc4a620fcff1cd22
AndersonAndrad <Anderson_Andrade_@outlook.com>
2020-04-27 16:43:41 -0300

    {FIXED: issue with the api call}

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 import api from '../../services/api';
+import { FormatPrice } from '../../util/format';
 
 export default class Home extends Component {
   state = {
@@ -10,8 +11,12 @@ export default class Home extends Component {
   };
 
   async componentDidMount() {
-    const response = await api.get('stock');
-    this.setState({ itemProducts: response.data });
+    const response = await api.get('products');
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormattedBRL: FormatPrice(product.price),
+    }));
+    this.setState({ itemProducts: data });
   }
 
   render() {
@@ -20,7 +25,18 @@ export default class Home extends Component {
     return (
       <ProductList>
         {itemProducts.map((product) => (
-          <li key={product.id}></li>
+          <li key={product.id}>
+            <img src={product.image} alt="" />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormattedBRL}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />
+              </div>
+              <span>Add to card</span>
+            </button>
+          </li>
         ))}
       </ProductList>
     );
