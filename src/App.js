48fc7eec56432ec2600219a0e7689d5b92d8a2c9
AndersonAndrad <Anderson_Andrade_@outlook.com>
2020-04-30 18:57:24 -0300

    {UPDATE: adding the history in app }

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './router';
+import { Router } from 'react-router-dom';
+import Routes from './router';
 import GlobalStyles from './styles/global';
 import Header from './components/Header';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import './config/reactotronConfig';
 import store from './store/index';
+import history from './services/history';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
-        <Router />
+        <Routes />
         <GlobalStyles />
-      </BrowserRouter>
+        <ToastContainer autoClose={3000} />
+      </Router>
     </Provider>
   );
 }
